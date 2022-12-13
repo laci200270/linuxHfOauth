dce30cf72220d3d6d212d0f568a9816427a9f01a
laci200270 <laci200270@gmail.com>
2022-12-13 08:30:23 +0100

    Add generic TokenValidator

---
+++ TokenValidator.h
@@ -0,0 +1,27 @@
+
+#ifndef LINUXHAZI_TOKENVALIDATOR_H
+#define LINUXHAZI_TOKENVALIDATOR_H
+
+#include <spdlog/logger.h>
+
+class TokenValidator {
+protected:
+    std::vector<spdlog::sink_ptr> logSinks;
+    spdlog::logger logger;
+
+    TokenValidator(const std::vector<spdlog::sink_ptr> &logSinks,
+                   const std::string loggerName = "GenericTokenValidator") : logSinks(logSinks),
+                                                                             logger(loggerName) {
+        logger.sinks().insert(logger.sinks().end(), logSinks.begin(), logSinks.end());
+    };
+
+public:
+    virtual bool validateClientSecret(const std::string &clientId, const std::string &clientSecret) = 0;
+
+    virtual bool validateRefreshToken(const std::string &userName, const std::string &refreshToken) = 0;
+
+    virtual bool validateAccessTokenForScope(const std::string &userName, const std::string &accessToken,
+                                             const std::string &scope) = 0;
+};
+
+#endif //LINUXHAZI_TOKENVALIDATOR_H
