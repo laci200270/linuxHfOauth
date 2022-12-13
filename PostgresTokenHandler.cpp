3c2bc20552b1513a0699e7e49570722bee3caca2
laci200270 <laci200270@gmail.com>
2022-12-13 09:42:28 +0100

    Remove unneeded fucntion

--- PostgresTokenHandler.cpp
+++ PostgresTokenHandler.cpp
@@ -3,20 +3,8 @@
 //
 
 #include <pqxx/transaction>
+#include <pqxx/nontransaction>
 #include "PostgresTokenHandler.h"
-
-bool PostgresTokenHandler::validateClientSecret(const std::string &clientId, const std::string &clientSecret) {
-    return false;
-}
-
-bool PostgresTokenHandler::validateRefreshToken(const std::string &userName, const std::string &refreshToken) {
-    return false;
-}
-
-bool PostgresTokenHandler::validateAccessTokenForScope(const std::string &userName, const std::string &accessToken,
-                                                       const std::string &scope) {
-    return TokenHandler::validateAccessTokenForScope(userName, accessToken, scope);
-}
 
 PostgresTokenHandler::PostgresTokenHandler(std::string jwtSecret, pqxx::connection &connection,
                                            std::vector<spdlog::sink_ptr> logSinks)
