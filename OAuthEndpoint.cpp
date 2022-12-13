a414046293d361dd1ee1ecc2ed7bde72d0b224eb
laci200270 <laci200270@gmail.com>
2022-12-13 02:13:48 +0100

    Add basic callback prototypes for authorize and token paths

--- OAuthEndpoint.cpp
+++ OAuthEndpoint.cpp
@@ -1,6 +1,13 @@
 #include "OAuthEndpoint.h"
 
-OAuthEndpoint::OAuthEndpoint(LoginValidation::LoginValidator &loginValidator): loginValidator(loginValidator){
-//    loginValidator.checkCredentials();
+OAuthEndpoint::OAuthEndpoint(LoginValidation::LoginValidator &loginValidator) : loginValidator(loginValidator) {
+}
+
+void OAuthEndpoint::authorizeCallback(const Pistache::Rest::Request &request, Pistache::Http::ResponseWriter response) {
+
+}
+
+void OAuthEndpoint::tokenCallback(const Pistache::Rest::Request &request, Pistache::Http::ResponseWriter response) {
+
 }
 
--- OAuthEndpoint.h
+++ OAuthEndpoint.h
@@ -1,12 +1,18 @@
 #ifndef LINUXHAZI_OAUTHENDPOINT_H
 #define LINUXHAZI_OAUTHENDPOINT_H
 
+#include <pistache/router.h>
 #include "LoginValidator.h"
 
 class OAuthEndpoint {
 
 public:
     OAuthEndpoint(LoginValidation::LoginValidator &loginValidator);
+
+    void authorizeCallback(const Pistache::Rest::Request &request, Pistache::Http::ResponseWriter response);
+
+    void tokenCallback(const Pistache::Rest::Request &request, Pistache::Http::ResponseWriter response);
+
 private:
     LoginValidation::LoginValidator &loginValidator;
 };
