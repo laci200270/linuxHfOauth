9d89baa4f68bcb005a3acb8c3cbeab5c70675876
laci200270 <laci200270@gmail.com>
2022-12-13 01:20:23 +0100

    Add minimal user representation

--- LoginValidator.h
+++ LoginValidator.h
@@ -11,6 +11,17 @@ namespace LoginValidation {
          * @return  true if credentials are correct, false otherwise
          */
         virtual bool checkCredentials(std::map<std::string, std::string> credentialMap) = 0;
+
+        /***
+         * Generic, minimal representation of an user
+         */
+        class User {
+        public:
+            ///The display name of the user
+            std::string displayName;
+            ///The email adddress of the user
+            std::string email;
+        };
     };
 }
 
